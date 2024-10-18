d1dd911d151fed071e6ae73004768817cd43103a
cao_guoqiang <cao_guoqiang@topsec.com.cn>
2024-10-18 18:34:13 +0800

    fix: tt

---
+++ python/__init__.py
@@ -0,0 +1,27 @@
+import io, os,subprocess
+import qrcode
+
+import requests
+ 
+# 图片的URL
+url = 'https://login.weixin.qq.com/qrcode/YfClsW9kSw=='
+ 
+# 发送GET请求获取图片内容
+response = requests.get(url)
+ 
+# 检查请求是否成功
+if response.status_code == 200:
+    # 打开文件进行二进制写入
+    with open('QR.png', 'wb') as f:
+        f.write(response.content)
+    print('图片保存成功')
+else:
+    print('请求图片失败，状态码:', response.status_code)
+
+ 
+# img = qrcode.make('https://login.weixin.qq.com/qrcode/YfClsW9kSw==')
+# img.save("QR.png")
+ 
+subprocess.call(['open', "QR.png"])
+
+ 
---
+++ python/settings.py
@@ -0,0 +1,18 @@
+import os
+
+
+# app_id
+APP_ID ="wx782c26e4c19acffb"
+
+# api key credential
+API_KEY = ""
+
+
+LANG ="zh_CN"
+# api url 
+HOST ="https://wx2.qq.com"
+LOGIN_HOST ="https://login.wx2.qq.com"
+
+#  https://login.wx2.qq.com/jslogin?appid=wx782c26e4c19acffb&fun=new&lang=zh_CN&redirect_uri=encodeURIComponent(https://wx2.qq.com/cgi-bin/mmwebwx-bin/webwxnewloginpage?mod=desktop)),
+API_Login =  HOST_URL + "/jslogin
+
